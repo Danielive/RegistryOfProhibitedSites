3ad0b8d8a4e00fed35ea310c89c4a13c58c77f77
Daniel <Danielive@outlook.com>
2017-09-14 00:40:30 +0300

    New

---
+++ src/main/java/live/daniel/study/MultiWebApplication.java
@@ -0,0 +1,12 @@
+package live.daniel.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiWebApplication.class, args);
+	}
+}
